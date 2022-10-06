3fb5626b22b1411f785933546f121f49f7c225f9
danillyamkin <danilwebdev@gmail.com>
2022-10-07 00:46:00 +0300

    Настроен Open Api

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   // create app
@@ -17,6 +18,15 @@ async function bootstrap() {
 
   // validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // open api (swagger)
+  const swagger = new DocumentBuilder()
+    .setTitle('Task manager')
+    .setDescription('Task manager app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
 
   // get port
   const port = config.get<number>('API_PORT') || 3333;
