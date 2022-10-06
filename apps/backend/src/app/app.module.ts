faa0692e0309bd411fe740146b5dad640ac018c8
danillyamkin <danilwebdev@gmail.com>
2022-10-07 00:15:57 +0300

    Изменен порядок импортов

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -4,6 +4,6 @@ import { PrismaModule } from '@task-manager/prisma';
 import { UserModule } from '@task-manager/user';
 
 @Module({
-  imports: [CoreModule, UserModule, PrismaModule],
+  imports: [CoreModule, PrismaModule, UserModule],
 })
 export class AppModule {}
